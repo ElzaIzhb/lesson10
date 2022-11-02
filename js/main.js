23eb9a3fb0c7c775e4a8b69fbc996808a5944133
Elza <aez7@ya.ru>
2022-11-02 21:43:33 +0300

    index-2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
-function showmessage() {
-  
+function showtext() {
+  document.getElementById("txt").style.opacity ="1";
+}
+function changecolor() {
+  document.getElementById("pict").style.border ="20px solid red";
+}
+function addpic() {
+  document.getElementById("pict").style.transform ="scale(3)";
+}
+function addwidth() {
+  document.getElementById("pict").style.width ="1100px";
+}
+function addheight() {
+  document.getElementById("pict").style.height ="700px";
+}
+//не получилось
+function chabgeborder() {
+  document.getElementById("pict").style.borderRadius ="50px";
+}
+//не получилось
+function hidden() {
+  document.getElementById("pict").style.display ="none";
+  document.getElementsByClassName('pic')[0].style.display = "none"
 }
 
 
+function defolt() {
+  window.location.reload();
+}
